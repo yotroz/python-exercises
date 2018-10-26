6dc903af2ce9a781446b1ba1f80a8586e7eb485c
Octavio <s.octavio90@gmail.comgit config --global user.email s.octavio90@gmail.comgit congfig --global user.name s.octavio90@gmail.comgit config --global user.name s.octavio90@gmail.com>
2018-10-26 14:43:31 +0200

    created a new line

---
+++ exercises.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 26 14:36:04 2018
+
+@author: yotroz
+"""
+
+def linear(lst, number): 
+    for i in range(len(lst)): 
+        if lst[i] == number: 
+            return i
+        
+    return None     
+    

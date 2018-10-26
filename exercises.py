7be3bb85aea39ee1d40a969887b3395d4e364f6a
Octavio <s.octavio90@gmail.comgit config --global user.email s.octavio90@gmail.comgit congfig --global user.name s.octavio90@gmail.comgit config --global user.name s.octavio90@gmail.com>
2018-10-26 14:44:32 +0200

    changed "number" for "num"
    in th parameter for the linear function

--- exercises.py
+++ exercises.py
@@ -6,10 +6,12 @@ Created on Fri Oct 26 14:36:04 2018
 @author: yotroz
 """
 
-def linear(lst, number): 
+def linear(lst, num): 
     for i in range(len(lst)): 
         if lst[i] == number: 
             return i
         
     return None     
+#%%
+
     
